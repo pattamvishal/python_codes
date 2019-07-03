8501f0cd628d8329d0f9ecb1a82362e990f6d95d
pattam vishal kumar <pattamvishal@gmail.com>
2019-07-03 23:39:50 +0530

    Update malefemalepredictionlist.py

--- malefemalepredictionlist.py
+++ malefemalepredictionlist.py
@@ -4,11 +4,12 @@ x=[[177,67,44],[177,70,43],[160,60,38],[166,65,40],[190,90,47],[175,65,39],[177,
 y=['male','female','male','female','female','male','male']
 clf=tree.DecisionTreeClassifier()
 clf=clf.fit(x,y)
-
+#here we take input from keyboard 
 a=int(input("enter the height:"))
 b=int(input("enter the weight:"))
 c=int(input("enter the shoe size:"))
 print(a,b,c)
+
 list_a =[]
 list_a.append(a)
 list_a.append(b)
